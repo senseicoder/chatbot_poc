4820beb548e7574b6073cb960fee94dd540d7237
Cédric Girard <cedric@daneel.net>
2017-03-27 18:45:48 +0200

    correction bug sur l'existence du fichier de stockage

--- code/comptes.inc.php
+++ code/comptes.inc.php
@@ -6,24 +6,30 @@ class CComptes
 
 	protected function _getPathFileData()
 	{
-		return CConfChatbot::pathLibXMPP . '/' . sha1($this->_sFrom);
+		return CConfChatbot::pathData . '/' . sha1($this->_sFrom);
+	}
+
+	protected function _initData()
+	{
+		echo __METHOD__ . "\n";
+		$this->_aData = array('from'=>$this->_sFrom, 'historique'=>array());
 	}
 
 	function __construct($sFrom)
 	{
 		$this->_sFrom = $sFrom;
 		$pathFileData = $this->_getPathFileData();
-		if(file_exists($pathFileData)) {
-			$this->_aData = array('from'=>$sFrom, 'historique'=>array());
+		if( ! file_exists($pathFileData)) {
+			$this->_initData();
 		}
 		else {
-			$this->_aData = json_decode(file_get_contents($pathFileData));
+			$this->_aData = json_decode(file_get_contents($pathFileData), TRUE);
+			if(empty($this->_aData)) $this->_initData();
 		}
 	}
 
 	function __destruct()
 	{
-		echo __METHOD__;
 		$pathFileData = $this->_getPathFileData();
 		file_put_contents($pathFileData, json_encode($this->_aData));
 	}
